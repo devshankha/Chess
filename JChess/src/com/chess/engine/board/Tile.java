aa60453334f4ea65f21dcc3f51b6471767ec55a5
devshankha <45039485+devshankha@users.noreply.github.com>
2019-01-26 22:30:23 +0530

    Add files via upload

---
+++ JChess/src/com/chess/engine/board/Tile.java
@@ -0,0 +1,80 @@
+package com.chess.engine.board;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.chess.engine.pieces.Piece;
+import com.google.common.*;
+import com.google.common.collect.ImmutableMap;
+
+public abstract class Tile {
+	final int tileCoordinates;
+
+	private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
+
+	Tile(int coordinates) {
+		tileCoordinates = coordinates;
+	}
+
+	private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+		Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+		for (int i = 0; i < 64; i++) {
+			emptyTileMap.put(i, new EmptyTile(i));
+
+		}
+		return ImmutableMap.copyOf(emptyTileMap);
+
+	}
+	
+	public static Tile createTile(final int coordinates, Piece p) {
+		return p != null? new OccupiedTile(coordinates, p):EMPTY_TILES_CACHE.get(coordinates);
+		
+	}
+
+	public abstract boolean isOccupied();
+
+	public abstract Piece getPiece();
+
+	public static final class EmptyTile extends Tile {
+
+		private EmptyTile(int coordinates) {
+			super(coordinates);
+
+		}
+
+		@Override
+		public boolean isOccupied() {
+
+			return false;
+		}
+
+		@Override
+		public Piece getPiece() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+	public static final class OccupiedTile extends Tile {
+
+		final Piece piece;
+
+		private OccupiedTile(int coordinates, Piece p) {
+			super(coordinates);
+			this.piece = p;
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public boolean isOccupied() {
+			return true;
+		}
+
+		@Override
+		public Piece getPiece() {
+			return null;
+		}
+	}
+
+}
---
+++ JChess/src/com/chess/engine/pieces/Piece.java
@@ -0,0 +1,22 @@
+package com.chess.engine.pieces;
+
+import java.util.List;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+public abstract class Piece {
+	protected final int piecePosition;
+	protected final Alliance pieceAlliance;
+	public Piece(final int piecePosition, final Alliance pieceAlliance) {
+		super();
+		this.piecePosition = piecePosition;
+		this.pieceAlliance = pieceAlliance;
+	}
+	public abstract List<Move> calculateLegalMoves(final Board board);
+	
+	
+	
+
+}
